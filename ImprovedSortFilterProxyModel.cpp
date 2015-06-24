ccc7789810f07161b86757b04d98e6ba4aab261e
hinst <hinstance@yandex.ru>
2015-06-24 15:57:56 +0300

    filtered sorting now works more or less

--- ImprovedSortFilterProxyModel.cpp
+++ ImprovedSortFilterProxyModel.cpp
@@ -8,20 +8,29 @@ void ImprovedSortFilterProxyModel::setFilterFixedString(const QString &pattern)
 {
 	this->QSortFilterProxyModel::setFilterFixedString(pattern);
 	this->currentFilterFixedString = pattern;
+	auto model = this->sourceModel();
+	this->setSourceModel(model);
 }
 
 bool ImprovedSortFilterProxyModel::lessThan(const QModelIndex &left, const QModelIndex &right) const
 {
-//	auto result = false;
-//	QVariant leftData = sourceModel()->data(left);
-//	QVariant rightData = sourceModel()->data(right);
-//	if (leftData.type() == QVariant::String && rightData.type() == QVariant::String)
-//	{
-//		auto leftString = leftData.toString();
-//		auto rightString = rightData.toString();
-//		result = leftString < rightString;
-//	}
-//	return result;
-	return this->QSortFilterProxyModel::lessThan(left, right);
+	auto result = this->QSortFilterProxyModel::lessThan(left, right);
+	if (currentFilterFixedString.length() > 0)
+	{
+		QVariant leftData = sourceModel()->data(left);
+		QVariant rightData = sourceModel()->data(right);
+		if (leftData.type() == QVariant::String && rightData.type() == QVariant::String)
+		{
+			auto leftString = leftData.toString();
+			auto rightString = rightData.toString();
+			auto leftIndex = leftString.indexOf(currentFilterFixedString, 0, Qt::CaseInsensitive);
+			auto rightIndex = rightString.indexOf(currentFilterFixedString, 0, Qt::CaseInsensitive);
+			if (leftIndex >= 0 && rightIndex >= 0)
+			{
+				result = leftIndex < rightIndex;
+			}
+		}
+	}
+	return result;
 }
 
